5c4a2a68249f229baa0bd496dc2639b81a723031
shireenrao <shireenrao@gmail.com>
2013-07-15 16:52:14 -0400

    Initial GUI design

---
+++ mp3tagstripper/main.py
@@ -0,0 +1,10 @@
+from PyQt4 import QtCore, QtGui
+import sys
+from ui.Mp3TagStripper import MainDialog
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    form = MainDialog()
+    form.show()
+    form.raise_()
+    sys.exit(app.exec_())
---
+++ mp3tagstripper/ui/Mp3TagStripper.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module implementing MainDialog.
+"""
+
+from PyQt4.QtGui import QDialog
+from PyQt4.QtCore import pyqtSignature
+
+from Ui_Mp3TagStripper import Ui_MainDialog
+
+class MainDialog(QDialog, Ui_MainDialog):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent = None):
+        """
+        Constructor
+        """
+        QDialog.__init__(self, parent)
+        self.setupUi(self)
+    
+    @pyqtSignature("")
+    def on_btnLoadDirectory_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        raise NotImplementedError
+    
+    @pyqtSignature("")
+    def on_btnRefresh_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        raise NotImplementedError
+    
+    @pyqtSignature("")
+    def on_btnSave_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        raise NotImplementedError
+    
+    @pyqtSignature("")
+    def on_btnFilterString_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        raise NotImplementedError
