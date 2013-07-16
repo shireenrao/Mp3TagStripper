0de7f03c24fc1e77e9d6fa3b36bbe7c09fb0465e
shireenrao <shireenrao@gmail.com>
2013-07-15 23:15:42 -0400

    Implement Load Dir

--- mp3tagstripper/ui/Mp3TagStripper.py
+++ mp3tagstripper/ui/Mp3TagStripper.py
@@ -4,48 +4,61 @@
 Module implementing MainDialog.
 """
 
-from PySide.QtGui import QDialog
-#from PyQt4.QtCore import pyqtSignature
+from PySide.QtCore import Slot
+from PySide.QtGui import QDialog,  QFileDialog
 
-from Ui_Mp3TagStripper import Ui_MainDialog
+from .Ui_Mp3TagStripper import Ui_MainDialog
+import os
 
 class MainDialog(QDialog, Ui_MainDialog):
     """
     Class documentation goes here.
     """
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         """
         Constructor
+        
+        @param parent reference to the parent widget (QWidget)
         """
         QDialog.__init__(self, parent)
         self.setupUi(self)
-
-#    @pyqtSignature("")
+    
+    @Slot()
     def on_btnLoadDirectory_clicked(self):
         """
         Slot documentation goes here.
         """
-        # TODO: not implemented yet
-        raise NotImplementedError
-
-#    @pyqtSignature("")
+        flags = QFileDialog.DontResolveSymlinks | QFileDialog.ShowDirsOnly
+        d = QFileDialog.getExistingDirectory(self, "Open Directory", os.getcwd(), flags)
+        #path, _ = QFileDialog.getOpenFileName(self, "Open File", os.getcwd())
+        self.lblDirName.setText(d)
+    
+    @Slot()
     def on_btnRefresh_clicked(self):
         """
         Slot documentation goes here.
         """
         # TODO: not implemented yet
         raise NotImplementedError
-
-#    @pyqtSignature("")
+    
+    @Slot()
     def on_btnSave_clicked(self):
         """
         Slot documentation goes here.
         """
         # TODO: not implemented yet
         raise NotImplementedError
-
-#    @pyqtSignature("")
+    
+    @Slot()
     def on_btnFilterString_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        raise NotImplementedError
+    
+    @Slot()
+    def on_btnStripString_clicked(self):
         """
         Slot documentation goes here.
         """
--- mp3tagstripper/ui/Ui_Mp3TagStripper.py
+++ mp3tagstripper/ui/Ui_Mp3TagStripper.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Mp3TagStripper.ui'
+# Form implementation generated from reading ui file '/Users/shireenrao/mydev/python/Mp3TagStripper/mp3tagstripper/ui/Mp3TagStripper.ui'
 #
-# Created: Mon Jul 15 17:18:56 2013
+# Created: Mon Jul 15 23:13:27 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -15,18 +15,21 @@ class Ui_MainDialog(object):
         MainDialog.resize(608, 440)
         self.gridLayout_2 = QtGui.QGridLayout(MainDialog)
         self.gridLayout_2.setObjectName("gridLayout_2")
-        self.btnLoadDirectory = QtGui.QPushButton(MainDialog)
-        self.btnLoadDirectory.setObjectName("btnLoadDirectory")
-        self.gridLayout_2.addWidget(self.btnLoadDirectory, 0, 0, 1, 1)
-        self.btnRefresh = QtGui.QPushButton(MainDialog)
-        self.btnRefresh.setObjectName("btnRefresh")
-        self.gridLayout_2.addWidget(self.btnRefresh, 0, 1, 1, 1)
-        self.btnSave = QtGui.QPushButton(MainDialog)
-        self.btnSave.setObjectName("btnSave")
-        self.gridLayout_2.addWidget(self.btnSave, 0, 2, 1, 1)
         self.lblDirName = QtGui.QLabel(MainDialog)
         self.lblDirName.setObjectName("lblDirName")
-        self.gridLayout_2.addWidget(self.lblDirName, 1, 0, 1, 2)
+        self.gridLayout_2.addWidget(self.lblDirName, 0, 0, 1, 1)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.btnLoadDirectory = QtGui.QPushButton(MainDialog)
+        self.btnLoadDirectory.setObjectName("btnLoadDirectory")
+        self.horizontalLayout.addWidget(self.btnLoadDirectory)
+        self.btnRefresh = QtGui.QPushButton(MainDialog)
+        self.btnRefresh.setObjectName("btnRefresh")
+        self.horizontalLayout.addWidget(self.btnRefresh)
+        self.btnSave = QtGui.QPushButton(MainDialog)
+        self.btnSave.setObjectName("btnSave")
+        self.horizontalLayout.addWidget(self.btnSave)
+        self.gridLayout_2.addLayout(self.horizontalLayout, 1, 0, 1, 1)
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
         self.frame = QtGui.QFrame(MainDialog)
@@ -35,9 +38,9 @@ class Ui_MainDialog(object):
         self.frame.setObjectName("frame")
         self.horizontalLayout_2 = QtGui.QHBoxLayout(self.frame)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.btnFilterString = QtGui.QPushButton(self.frame)
-        self.btnFilterString.setObjectName("btnFilterString")
-        self.horizontalLayout_2.addWidget(self.btnFilterString)
+        self.btnStripString = QtGui.QPushButton(self.frame)
+        self.btnStripString.setObjectName("btnStripString")
+        self.horizontalLayout_2.addWidget(self.btnStripString)
         self.radioButton = QtGui.QRadioButton(self.frame)
         self.radioButton.setEnabled(True)
         self.radioButton.setObjectName("radioButton")
@@ -55,18 +58,28 @@ class Ui_MainDialog(object):
         self.treeWidget.setObjectName("treeWidget")
         self.treeWidget.headerItem().setText(0, "1")
         self.gridLayout.addWidget(self.treeWidget, 1, 0, 1, 1)
-        self.gridLayout_2.addLayout(self.gridLayout, 2, 0, 1, 3)
+        self.gridLayout_2.addLayout(self.gridLayout, 2, 0, 1, 1)
 
         self.retranslateUi(MainDialog)
         QtCore.QMetaObject.connectSlotsByName(MainDialog)
 
     def retranslateUi(self, MainDialog):
         MainDialog.setWindowTitle(QtGui.QApplication.translate("MainDialog", "Mp3 Tag Stripper", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblDirName.setText(QtGui.QApplication.translate("MainDialog", "Please, load a mp3 directory", None, QtGui.QApplication.UnicodeUTF8))
         self.btnLoadDirectory.setText(QtGui.QApplication.translate("MainDialog", "Load Directory", None, QtGui.QApplication.UnicodeUTF8))
         self.btnRefresh.setText(QtGui.QApplication.translate("MainDialog", "Refresh", None, QtGui.QApplication.UnicodeUTF8))
         self.btnSave.setText(QtGui.QApplication.translate("MainDialog", "Save", None, QtGui.QApplication.UnicodeUTF8))
-        self.lblDirName.setText(QtGui.QApplication.translate("MainDialog", "Please, load a mp3 directory", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnFilterString.setText(QtGui.QApplication.translate("MainDialog", "Filter", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnStripString.setText(QtGui.QApplication.translate("MainDialog", "Strip", None, QtGui.QApplication.UnicodeUTF8))
         self.radioButton.setText(QtGui.QApplication.translate("MainDialog", "ID3", None, QtGui.QApplication.UnicodeUTF8))
         self.radioButton_2.setText(QtGui.QApplication.translate("MainDialog", "File", None, QtGui.QApplication.UnicodeUTF8))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainDialog = QtGui.QDialog()
+    ui = Ui_MainDialog()
+    ui.setupUi(MainDialog)
+    MainDialog.show()
+    sys.exit(app.exec_())
 
