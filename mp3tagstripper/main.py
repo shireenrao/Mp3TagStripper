2645fb1a244c8584533d318e0465e9ee6a32c39e
shireenrao <shireenrao@gmail.com>
2013-07-16 15:41:07 -0400

    Fix load dir

--- mp3tagstripper/main.py
+++ mp3tagstripper/main.py
@@ -5,11 +5,25 @@ import sys
 from ui import Ui_Mp3TagStripper
 import os
 
+__appname__ = "Mp3 Tag Stripper"
+
 class MainDialog(QDialog, Ui_Mp3TagStripper.Ui_MainDialog):
 
     def __init__(self, parent=None):
         super(MainDialog, self).__init__(parent)
         self.setupUi(self)
+
+        self.btnLoadDirectory.setFocus()
+        self.btnLoadDirectory.clicked.connect(self.loadDir)
+
+
+    def loadDir(self):
+        dir = "."
+        flags = QFileDialog.DontResolveSymlinks | QFileDialog.ShowDirsOnly
+        d = QFileDialog.getExistingDirectory(self, __appname__ +
+                                             " Open Directory", os.getcwd()
+                                             , flags)
+        self.lblDirName.setText(d)
 
 app = QApplication(sys.argv)
 form = MainDialog()
