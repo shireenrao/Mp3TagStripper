93bb8af9f5b08eee62f276dc92e19cf1e580e757
shireenrao <shireenrao@gmail.com>
2013-07-17 22:28:49 -0400

    Add file lookup and ID3 to eric4 implementation

--- mp3tagstripper-eric4/ui/Mp3TagStripper.py
+++ mp3tagstripper-eric4/ui/Mp3TagStripper.py
@@ -19,15 +19,18 @@ class MainDialog(QDialog, Ui_MainDialog):
         """
         QDialog.__init__(self, parent)
         self.setupUi(self)
-    
+
+        header = QTreeWidgetItem(["Directory", "Field", "Value"])
+        self.treeWidget.setHeaderItem(header)
+        self.btnLoadDirectory.setFocus()
+
     @pyqtSignature("")
     def on_btnLoadDirectory_clicked(self):
         """
         Slot documentation goes here.
         """
-        # TODO: not implemented yet
-        raise NotImplementedError
-    
+        self.loadDir()
+
     @pyqtSignature("")
     def on_btnRefresh_clicked(self):
         """
@@ -35,7 +38,7 @@ class MainDialog(QDialog, Ui_MainDialog):
         """
         # TODO: not implemented yet
         raise NotImplementedError
-    
+
     @pyqtSignature("")
     def on_btnSave_clicked(self):
         """
@@ -43,7 +46,7 @@ class MainDialog(QDialog, Ui_MainDialog):
         """
         # TODO: not implemented yet
         raise NotImplementedError
-    
+
     @pyqtSignature("")
     def on_btnStripString_clicked(self):
         """
@@ -51,3 +54,43 @@ class MainDialog(QDialog, Ui_MainDialog):
         """
         # TODO: not implemented yet
         raise NotImplementedError
+
+    def loadDir(self):
+        dir = "."
+        flags = QFileDialog.DontResolveSymlinks | QFileDialog.ShowDirsOnly
+        d = QFileDialog.getExistingDirectory(self, __appname__ +
+                                             " Open Directory", os.getcwd()
+                                             , flags)
+        if d != '':
+            self.treeWidget.clear()
+            self.lblDirName.setText(d)
+            self.mp3dir = d
+            fileList = self.getmp3list(d)
+            header = QTreeWidgetItem(["Directory", "Field", "Value"])
+            self.treeWidget.setHeaderItem(header)
+            rootdir = []
+            rootdir.append(os.path.basename(d))
+            root = QTreeWidgetItem(self.treeWidget, rootdir)
+            for file in fileList:
+                filename = os.path.basename(file)
+                audio = MP3(file, ID3=EasyID3)
+                myfile = []
+                myfile.append(filename)
+                mp3file = QTreeWidgetItem(root, myfile)
+                for key in sorted(audio.keys()):
+                    mp3detail = QTreeWidgetItem(mp3file)
+                    mp3detail.setText(0,"")
+                    mp3detail.setText(1,key)
+                    mp3detail.setText(2,audio[key][0])
+            self.treeWidget.resizeColumnToContents(0)
+            #pointListBox.show()
+
+    def getmp3list(self, path):
+        fileList = []
+        if os.path.exists(path):
+            for root, dirs, files in os.walk(path):
+                for name in files:
+                    filename = os.path.join(root,name)
+                    if filename.lower().endswith('.mp3'):
+                        fileList.append(filename)
+        return fileList
